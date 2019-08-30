96f1d3f8ecdb2edbdb173b73fa80cebca7381107
tinaClement <tinaslizz49@gmail.com>
2019-08-30 18:51:23 +0400

    made some changes

--- blog/js/script.js
+++
@@ -1,7 +0,0 @@
-const drop = document.querySelector('.drop');
-const navLinks = document.querySelector('.nav-links');
-const Links = document.querySelector('.nav-links li');
-
-drop.addEventListener('click',() =>{
-    navLinks.classList.toggle('open');
-})
